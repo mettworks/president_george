853f105325831d2d14aa56aff5cb173cad56bb99
André Lehmann <abcdefg@blacktux.de>
2013-08-06 23:17:27 +0200

    ADC Messung, Bullshit
    Hmmm...

--- main.c
+++ main.c
@@ -698,6 +698,56 @@ int keycheck(void)
 	}
 }
 
+/* ADC initialisieren */
+void ADC_Init(void) {
+ 
+  uint16_t result;
+ 
+  // 2,5V als Referenz
+  ADMUX = (1<<REFS0) | (1<<REFS1);    
+  
+  // Bit ADFR ("free running") in ADCSRA steht beim Einschalten
+  // schon auf 0, also single conversion
+  ADCSRA =  (1<<ADPS1) | (1<<ADPS0);     // Frequenzvorteiler
+  ADCSRA |= (1<<ADEN);                  // ADC aktivieren
+ 
+  /* nach Aktivieren des ADC wird ein "Dummy-Readout" empfohlen, man liest
+     also einen Wert und verwirft diesen, um den ADC "warmlaufen zu lassen" */
+ 
+  ADCSRA |= (1<<ADSC);                  // eine ADC-Wandlung 
+  while (ADCSRA & (1<<ADSC) ) {         // auf Abschluss der Konvertierung warten
+  }
+  /* ADCW muss einmal gelesen werden, sonst wird Ergebnis der nächsten
+     Wandlung nicht übernommen. */
+  result = ADCW;
+}
+
+/* ADC Einzelmessung */
+uint16_t ADC_Read( uint8_t channel )
+{
+  // Kanal waehlen, ohne andere Bits zu beeinflußen
+  ADMUX = (ADMUX & ~(0x1F)) | (channel & 0x1F);
+  ADCSRA |= (1<<ADSC);            // eine Wandlung "single conversion"
+  while (ADCSRA & (1<<ADSC) ) {   // auf Abschluss der Konvertierung warten
+  }
+  return ADCW;                    // ADC auslesen und zurückgeben
+}
+ 
+/* ADC Mehrfachmessung mit Mittelwertbbildung */
+/* beachte: Wertebereich der Summenvariablen */
+uint16_t ADC_Read_Avg( uint8_t channel, uint8_t nsamples )
+{
+  uint32_t sum = 0;
+ 
+  for (uint8_t i = 0; i < nsamples; ++i ) {
+    sum += ADC_Read( channel );
+  }
+ 
+  return (uint16_t)( sum / nsamples );
+}
+
+
+
 //
 // sehr unelegant, muss mit einem Timer gemacht werden
 // erstmal geht es nur um den HW Test ... ^^
@@ -910,7 +960,10 @@ int main(void)
 	_delay_ms(500);
 	
 	
-	
+	 uint16_t adcval;
+  ADC_Init();
+ 
+
 	
 	
   sei();
@@ -919,7 +972,17 @@ int main(void)
 	#endif
 	while(1)
 	{
-
+    adcval = ADC_Read(0);  // Kanal 0
+    // mach was mit adcval
+ 
+    adcval = ADC_Read_Avg(0, 4);  // Kanal 2, Mittelwert aus 4 Messungen
+    // mach was mit adcval
+			uint8_t string[20];
+		uart_puts("Messwert: ");
+		sprintf(string,"%u",adcval);
+		uart_puts(string);
+		uart_puts("\r\n");
+		_delay_ms(1000);
 	}
   return 0;
 } 
